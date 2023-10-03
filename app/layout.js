2fd76426ad060f5c008c77da9f4a9a104d6212fb
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-10-03 12:33:29 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Home|APSOH',
+  title: 'APSOH',
   description: 'The official school website',
 }
 
