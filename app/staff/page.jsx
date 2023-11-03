22cb2e937aff632e1b46085f1caa5618f0e872c1
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-11-03 22:13:00 +0000

    .

--- app/staff/page.jsx
+++ app/staff/page.jsx
@@ -3,15 +3,40 @@ import { StaffData } from '../components/StaffData'
 
 const page = () => {
   return (
-    <div className='p-5 md:p-10 md:pl-24 '>
-    <div className='flex flex-wrap gap-16 items-ceter justify-center md:justify-start'>
+    <div className='p-5 md:p-10'>
+    <div className='flex flex-wrap fle gap-5 items-center justify-center '>
         {
             StaffData.map((staff,index)=>(
-                <div key={index}>
-                    <h2>{staff.name}</h2>
-                    <div>
-                        <h1>Position:</h1>
-                        <span>{staff.position}</span>
+                <div key={index} className='py-4 px-4 pb-16 md:pb-5 border-2 shadow-xl flex flex-col gap-1 w-[80vw] md:w-fit'>
+                    <h2 className='text-lime-700 font-bold text-xl'>{staff.name}</h2>
+                    <div className='flex gap-2'>
+                        <h1 className='text-lime-600 font-bold text-sm'>Position:</h1>
+                        <span className='text-slate-700'>{staff.position}</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <h1 className='text-lime-600 font-bold text-sm'>Subjects:</h1>
+                        <span className='text-slate-700'>{staff.position}</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <h1 className='text-lime-600 font-bold text-sm '>Class:</h1>
+                        <span className='text-slate-700'>{staff.class  }</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <h1 className='text-lime-600 font-bold text-sm'>Status:</h1>
+                        <span className='text-slate-700'>{staff.status}</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <h1 className='text-lime-600 font-bold text-sm'>Phone:</h1>
+                        <span className='text-slate-700'>{staff.phone}</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <h1 className='text-lime-600 font-bold text-sm'>Email:</h1>
+                        <span className='text-slate-700'>{staff.email}</span>
                     </div>
 
                 </div>
